432d6382f659edbaaaa85d53eaf71b006eb4e8fc
Enzo <enhance674@gmail.com>
2025-06-24 15:08:58 +0200

    update exception

--- src/main/java/org/example/exercice2_rest/exception/NotFoundException.java
+++ src/main/java/org/example/exercice2_rest/exception/NotFoundException.java
@@ -1,5 +1,11 @@
 package org.example.exercice2_rest.exception;
 
 public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
 
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
